ec2675dfb58212cce49c1ed0a6222ec1da503c12
Artur Cempura <artur.cempura@grandparade.co.uk>
2024-07-27 16:58:07 +0200

    basic-ui-of-the-page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,21 @@
+import "./App.scss";
+
+export default function HomePage() {
+  return (
+    <div className="main-content">
+      <h1>Home</h1>
+      <h2>LORD-STAL</h2>
+      <h2>KONSTRUCKJE STALOWE</h2>
+
+      <p>
+        Nowoczesne garaże dopasowane pod Twoje potrzeby wykonane w całości z
+        profili zamkniętych ocynkowanych.
+      </p>
+      <p>
+        Proponujemy bogatą ofertę garaży blaszanych o szerokiej gamie kolorów i
+        dodatków.
+      </p>
+      <p>Zapewniamy najwyższą jakość wykonania.</p>
+    </div>
+  );
+}
